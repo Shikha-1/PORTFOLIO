8e3d0e777ae5747a8e0666486a0acc1f49b288fc
animesh singh <animeshsingh692@gmail.com>
2023-09-24 20:11:20 +0530

    Changes: Fixed css and made required changes to showcase other things

--- src/component/ServiceSection.js
+++ src/component/ServiceSection.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import "../css/Services.css"
 
-function ServiceSection({text, title}) {
-    return (
-      <div className="ServicesSection">
-        <div className="service">
-          <div className="service-content">
-            <h5 className="s-title">{title}</h5>
-            <p className="s-text">{text}</p>
+function ServiceSection({ text, title, role, timeSpan }) {
+  return (
+    <div className="ServicesSection">
+      <div className="service">
+        <div className="service-content">
+          <h5 className="s-title">{title}</h5>
+          <div className='company'>
+            <div className='service-section'>
+              {role}
+            </div>
+            <div className='service-section'>{timeSpan}</div>
           </div>
+          <p className="s-text">Responsibilities:</p>
+          <ul className="s-description">{text.split("*")?.map(task => <li key={task}>{task}</li>)}</ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default ServiceSection
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,38 +3,60 @@ import "../css/About.css"
 import Title from "../component/Title"
 import SkillSection from "../component/SkillSection"
 import ServiceSection from "../component/ServiceSection"
-import ImageSection from "../component/ImageSection"
-
+import EducationSection from "../component/EducationSection"
 function About() {
-    return (
-      <>
-        <Title title={"About Me"} span={"About Me"} />
-        <ImageSection />
-        <Title title={"My Skills"} span={"My Skills"} />
-        <div className="skillsContainer">
-          <SkillSection skill={"HTML"} progress={"85%"} width={"85%"} />
-          <SkillSection skill={"CSS"} progress={"70%"} width={"70%"} />
-          <SkillSection skill={"BOOTSRAP"} progress={"90%"} width={"90%"} />
-          <SkillSection skill={"React Js"} progress={"70%"} width={"70%"} />
-          <SkillSection skill={"Javascript"} progress={"80%"} width={"80%"} />
-          <SkillSection skill={"Node Js"} progress={"80%"} width={"80%"} />
-          <SkillSection skill={"Express Js"} progress={"70%"} width={"70%"} />
-          <SkillSection skill={"MongoDB"} progress={"70%"} width={"70%"} />
-          <SkillSection skill={"Python"} progress={"70%"} width={"70%"} />
-        </div>
-        <Title title={"What I Can Do"} span={"What I Can Do"} />
-        <div className="servives-container">
-          <ServiceSection
-            title={"Frontend Development"}
-            text={"Can develop web pages using HTML, CSS, Object oriented Javascript, ES6 & JSON. Good understanding of Document Object Model(DOM) and DOM functions."}
-         />
-          <ServiceSection
-            title={"Full Stack Development"}
-            text={"Good understanding of Back-end API development using NodeJs. Ability to design and develop innovative applications by utilizing a range of technologies, including React, NodeJs, ExpressJs & MongoDB."}
-          />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <Title title={"Work Experience"} />
+      <div className="servives-container">
+        <ServiceSection
+          role="Software Devewlopment Engineer"
+          timeSpan="Feb 2022 - Present"
+          title="ZopSmart"
+          text="Built and maintained a high volume (500+) of responsive
+                and interactive websites using React, Context-API, and
+                SCSS. * Consumed and rendered listing type data specific for a
+                company profile served from a five-point REST service.
+                * Implemented Google Analytics and Google tag manager
+                into applications for event tracking. * Built custom React components for the user-facing features
+                using styled components. * Implemented stateful and
+                stateless components app design patterns into the design
+                of the new web portal.
+                * Involved with bug fixing on functionality, performance, and
+                designing issues. Also, Worked closely with team members
+                to review code for best practices and standards. * Worked with design and UX teams to improve usability and
+                design of products and also collaborated with project &
+                product managers in setting goals & scoping projects."
+        />
+        <ServiceSection
+          role="Junior Web Developer"
+          timeSpan="Sep 2021 - Feb 2022"
+          title="GNX Group"
+          text="Worked as a front end developer and was involved in the
+                analysis of requirements, design, development, integration, deployment, and testing. * Utilised HTML, css. and material-ui within react framework
+                to create a responsive user interface. * Build storyboards to conceptualise designs and convey
+                project plans to clients and management."
+        />
+      </div>
+      <div className="img-section">
+        <Title title={"Educational Qualifications"} />
+        <EducationSection course={"B.Tech. in Computer Science & Engineering (2018 - 2022)"} marksObtained={"8.8"} marksParam={"CGPA"} location={"New Delhi"} institute={"Guru Govind Singh Indraprastha University"} />
+        <EducationSection course={"Senior Secondary"} marksObtained={80} marksParam={"% Obtained"} location={"Saran, Bihar"} institute={"J D Public School"} />
+        <EducationSection course={"Secondary"} marksObtained={95} marksParam={"% Obtained"} location={"Saran, Bihar"} institute={"Galaxy Residential Public School"} />
+      </div>
+      <Title title={"My Skills"} />
+      <div className="skillsContainer">
+        <SkillSection skill={"HTML"} progress={"95%"} width={"95%"} />
+        <SkillSection skill={"CSS"} progress={"90%"} width={"90%"} />
+        <SkillSection skill={"BOOTSRAP"} progress={"95%"} width={"95%"} />
+        <SkillSection skill={"React Js"} progress={"90%"} width={"90%"} />
+        <SkillSection skill={"Javascript"} progress={"80%"} width={"80%"} />
+        <SkillSection skill={"Node Js"} progress={"70%"} width={"70%"} />
+        <SkillSection skill={"Express Js"} progress={"70%"} width={"70%"} />
+        <SkillSection skill={"MongoDB"} progress={"70%"} width={"70%"} />
+      </div>
+    </>
+  );
 }
 
 export default About
